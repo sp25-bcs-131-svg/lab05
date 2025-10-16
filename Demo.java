468e20c57313620deb6acd09124ac9eda0cf3a94
sp25-bcs-131-svg <sp25-bcs-131@cuilahore.edu.pk>
2025-10-17 01:51:43 +0500

    Delete Demo.java

--- Demo.java
+++
@@ -1,8 +0,0 @@
-public class Demo{
-	public static void main(String args[]){
-		CityCinema cinema  = new CityCinema("Lahore",2);
-		Cinema c = new Cinema ("Cinepax",4);
-		c.displayDetails();
-		cinema.displayCinema();
-	}
-}
